969a78fbc3ea7c9460b165ea22fa1542ebd7335c
Fergal Moylan <fergalmoylan@MacBook-Air.station>
2024-11-25 12:20:29 +0000

    Create simple firehose stream

---
+++ src/cid_compat.rs
@@ -0,0 +1,27 @@
+use cid::{multihash::Multihash, Cid};
+
+pub struct CidOld(cid_old::Cid);
+
+
+
+// from https://github.com/sugyan/atrium/tree/main/examples/firehose
+impl From<cid_old::Cid> for CidOld {
+    fn from(value: cid_old::Cid) -> Self {
+        Self(value)
+    }
+}
+impl TryFrom<CidOld> for Cid {
+    type Error = cid::Error;
+    fn try_from(value: CidOld) -> Result<Self, Self::Error> {
+        let version = match value.0.version() {
+            cid_old::Version::V0 => cid::Version::V0,
+            cid_old::Version::V1 => cid::Version::V1,
+        };
+
+        let codec = value.0.codec();
+        let hash = value.0.hash();
+        let hash = Multihash::from_bytes(&hash.to_bytes())?;
+
+        Self::new(version, codec, hash)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+mod frames;
+mod cid_compat;
+
+use anyhow::{Result};
+use atrium_api::app::bsky::feed::post::Record;
+use atrium_api::com::atproto::sync::subscribe_repos::{Commit, NSID};
+use atrium_api::types::{CidLink, Collection};
+use cid_compat::CidOld;
+use frames::Frame;
+use futures::StreamExt;
+use tokio_tungstenite::tungstenite::Message;
+use tokio_tungstenite::{connect_async};
+use serde_json;
+
+
+const FIREHOSE_DOMAIN: &str = "bsky.network";
+
+async fn handle_commit(commit: &Commit) ->Result<()> {
+    let did = &commit.repo.as_str();
+    for op in &commit.ops {
+        let collection = op.path.split('/').next().expect("op.path is empty");
+        if op.action == "create" && collection == atrium_api::app::bsky::feed::Post::NSID  {
+            let (items, _) = rs_car::car_read_all(&mut commit.blocks.as_slice(), true).await?;
+            if let Some((_, item)) = items.iter().find(|(cid, _)| {
+                let cid = CidOld::from(*cid).try_into().expect("couldn't convert old to new cid");
+                Some(CidLink(cid)) == op.cid
+            }) {
+                let record = serde_ipld_dagcbor::from_reader::<Record, _>(&mut item.as_slice())?;
+                println!("{record:#?}")
+            }
+        }
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let (mut stream, _) = connect_async(format!("wss://{FIREHOSE_DOMAIN}/xrpc/{NSID}")).await?;
+    while let Some(result) = {
+        if let Some(Ok(Message::Binary(data))) = stream.next().await {
+            Some(Frame::try_from(data.as_slice()))
+        } else {
+            None
+        }
+    } {
+        if let Ok(Frame::Message(Some(t), message)) = result {
+            if t.as_str() == "#commit" {
+                let commit = serde_ipld_dagcbor::from_reader(message.body.as_slice())?;
+                if let Err(err) = handle_commit(&commit).await {
+                    eprintln!("FAILED: {err:?}");
+                }
+            }
+        }
+    }
+    Ok(())
 }
